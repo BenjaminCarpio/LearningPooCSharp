fb880983aca4900ccfe7be03ef6195326535308b
BenjaminCarpio <00205619@uca.edu.sv>
2020-05-01 22:00:22 -0600

    Add: Semana 7 material

---
+++ Semana07/Parte practica/Semana07_Ejercicio01/Semana07_Ejercicio01/Banco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Semana07_Ejercicio01
+{
+    public class Banco
+    {
+        public static void registrarTarjeta(){
+            Console.Write("\nNúmero de tarjeta: ");
+            string numero = Console.ReadLine();
+
+            if (numero != null && (numero.Length == 11 && numero[3] .Equals('-') && numero[7] .Equals('-')))
+            {
+                GestorArchivos.Anexar("Tarjetas.txt", numero);
+                Console.WriteLine("Tarjeta creada exitosamente!");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese una tarjeta valida.\nUse el formato siguiente:\nxxx-xxx-xxx\n");
+            }
+        }
+    
+        public static void consultarTarjetas(){
+            Console.WriteLine("\n" + GestorArchivos.Leer("Tarjetas.txt"));
+        }
+        public static bool realizarCompras(string tarjeta){
+            return GestorArchivos.Buscar("Tarjetas.txt", tarjeta);
+        }
+    }
+}
---
+++ Semana07/Parte practica/Semana07_Ejercicio01/Semana07_Ejercicio01/ConsolaJuegos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Semana07_Ejercicio01
+{
+    public class ConsolaJuegos
+    {
+        public static void comprarJuego(){
+                Console.Write("\nNúmero de tarjeta: ");
+                string numero = Console.ReadLine();
+        
+                if(Banco.realizarCompras(numero)){
+                    Console.Write("\nNombre del juego: ");
+                    string juego = Console.ReadLine();
+            
+                    GestorArchivos.Anexar("Juegos.txt", juego);
+            
+                    Console.WriteLine("\nJuego comprado exitosamente!");
+                }
+            }
+    
+            public static void jugar(){
+                Console.Write("Nombre del juego: ");
+                string juego = Console.ReadLine();
+        
+                if(GestorArchivos.Buscar("Juegos.txt", juego))
+                    Console.WriteLine("Listo para jugar!");
+                else
+                    Console.WriteLine("Primero debe comprar el juego :(");
+            }
+    }
+}
